a2f13f12936fa87b276222c6c017cd55cb8b150c
Paul Mackenzie <paul@whatspauldoing.com>
2018-02-13 19:00:54 +0000

    Adding Gulp clean task

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,16 @@
 var gulp = require('gulp');
 var cleanSketch = require('gulp-clean-sketch');
+var clean = require('gulp-clean-fix');
 
 gulp.task('cleanSketch', function() {
   return gulp.src('img/svg/*.svg')
-  .pipe(cleanSketch())
-  .pipe(gulp.dest('img/svg/'));
+    .pipe(cleanSketch())
+    .pipe(gulp.dest('img/svg/'));
+});
+
+gulp.task('clean', function() {
+  return gulp.src('*/.DS_Store', {read : false})
+    .pipe(clean());
 });
 
 gulp.task('default');
