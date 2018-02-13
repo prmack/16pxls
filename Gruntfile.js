9849a48e94ba9d09456e076e70f04af85ad2a852
Paul Mackenzie <paul@whatspauldoing.com>
2018-02-13 14:21:56 +0000

    Adding img folder

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,33 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    clean : ['.DS_Store', '**/.DS_Store']
+    clean : ['.DS_Store', '**/.DS_Store'],
+    svg2png: {
+      all : {
+        files : [{
+            src : ['img/svg/*.svg'],
+            dest : 'img/png'
+        }]
+      }
+    },
+    pngmin : {
+      compile : {
+        options : '.png',
+        force : true
+      },
+      files: [
+        {
+          src : 'img/png/*.png',
+          dest : 'img/png'
+        }
+      ]
+    }
   });
 
   grunt.registerTask('default');
-
+  
   // Load Tasks
   grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-pngmin');
+  grunt.loadNpmTasks('grunt-svg-to-png');
 }
