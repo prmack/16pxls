a4fbd11c19a01d7c485daf744023aaa840f49646
Paul Mackenzie <paul@whatspauldoing.com>
2018-02-14 15:44:56 +0000

    Creating Icon Process for Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,30 @@
 var gulp = require('gulp');
+var del = require('del');
+var clean = require('gulp-clean');
 var cleanSketch = require('gulp-clean-sketch');
-var clean = require('gulp-clean-fix');
+var svgmin = require('gulp-svgmin');
+var raster = require('gulp-raster');
+var imagemin = require('gulp-imagemin');
+var rename = require('gulp-rename');
 
-gulp.task('cleanSketch', function() {
-  return gulp.src('img/svg/*.svg')
+gulp.task('processIcons', function() {
+  return gulp.src('dist/svg/*.svg')
+    // Clean Sketch Meta
     .pipe(cleanSketch())
-    .pipe(gulp.dest('img/svg/'));
+    // Minimise SVG
+    .pipe(svgmin())
+    .pipe(gulp.dest('dist/svg/'))
+    // Convert to .PNG
+    .pipe(raster())
+    .pipe(rename({extname : '.png'}))
+    // Optimise PNG
+    .pipe(imagemin([imagemin.optipng({optimizationLevel: 5})]))
+    .pipe(gulp.dest('dist/png/'));
 });
 
-gulp.task('clean', function() {
-  return gulp.src('*/.DS_Store', {read : false})
-    .pipe(clean());
+gulp.task('clean', function(){
+  return del(['**/.DS_Store']);
 });
+
 
 gulp.task('default');
